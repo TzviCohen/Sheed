81406eba59237edfe72b4156c4ae79cd589d30be
or.gershoni <or.gershoni@cs.huji.ac.il>
2021-08-14 22:28:26 +0300

    updated some SheedUser fields

--- app/src/main/java/com/postpc/Sheed/SheedUser.java
+++ app/src/main/java/com/postpc/Sheed/SheedUser.java
@@ -3,7 +3,9 @@ package com.postpc.Sheed;
 import android.media.Image;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 enum Gender
 {
@@ -23,5 +25,26 @@ public class SheedUser implements Serializable {
 
     public List<String> community;
 
+    public List<String> matches;
+    public List<String> matchesMade;
+    String id;
+
+    SheedUser(String firstName, String lastName, Integer age, Gender gender, Gender interestedIn)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.interestedIn = interestedIn;
+
+        // think about image is handled
+
+        id = UUID.randomUUID().toString();
+
+        // empty lists
+        community = new ArrayList<>();
+        matches = new ArrayList<>();
+        matchesMade = new ArrayList<>();
+    }
 
 }
