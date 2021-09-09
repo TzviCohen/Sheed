d88f4b6b69a776ebe94076921416d55a7ce352bc
sagiv-yatom <sagiv.yatom@cs.huji.ac.il>
2021-09-09 13:57:25 +0300

    implement YourMatchesFragment file

--- app/src/main/java/com/postpc/Sheed/yourMatches/YourMatchesFragment.java
+++ app/src/main/java/com/postpc/Sheed/yourMatches/YourMatchesFragment.java
@@ -3,16 +3,23 @@ package com.postpc.Sheed.yourMatches;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.postpc.Sheed.Query;
 import com.postpc.Sheed.R;
 import com.postpc.Sheed.SheedApp;
 import com.postpc.Sheed.SheedUser;
 import com.postpc.Sheed.database.SheedUsersDB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,9 +31,7 @@ public class YourMatchesFragment extends Fragment {
     private SheedUser sheedUser;
     SheedUsersDB db;
 
-    public YourMatchesFragment() {
-        // Required empty public constructor
-    }
+    public YourMatchesFragment() {}
 
     /**
      * Use this factory method to create a new instance of
@@ -52,10 +57,8 @@ public class YourMatchesFragment extends Fragment {
             }
             sheedUser = db.currentSheedUser;
         }
-//        // in case you want to get some arguments:
-//        if (getArguments() != null) {
-//            // sheedUser = (SheedUser) getArguments().getSerializable(SHEED_USER);
-//        }
+
+        Log.i("User Name", sheedUser.firstName + " " + sheedUser.lastName);
     }
 
     @Override
@@ -63,8 +66,15 @@ public class YourMatchesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_your_matches, container, false);
-        TextView textUserId = view.findViewById(R.id.your_matches_header_title);
-        textUserId.setText("user retrieved from database: " + sheedUser.firstName + " " + sheedUser.lastName);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerYourMatchesList);
+
+        List<String> userIdMatches = sheedUser.matches;
+        List<String> matchMadeBy = sheedUser.matchesMade;
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        YourMatchesAdapter adapter = new YourMatchesAdapter(getActivity(), userIdMatches, matchMadeBy);
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
 }
